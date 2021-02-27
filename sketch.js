d077826cd7eeb6bd03056f9e4d2dd77793b9a310
AKSHAT-SINGH-CHAUHAN-007 <79372526+AKSHAT-SINGH-CHAUHAN-007@users.noreply.github.com>
2021-02-27 16:03:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score, ground
+var survivalTime
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+  FoodGroup= new Group()
+  obstacleGroup= new Group()
+  
+}
+
+
+
+function setup() {
+  createCanvas(670, 400);
+  score=0
+  survivalTime=0
+  
+  ground=createSprite(0,400,1500,10)
+  
+   monkey=createSprite(90,370,10,10)
+  monkey.addAnimation("monkey_running",monkey_running)
+  monkey.scale=0.1
+  
+  
+  
+
+  }
+function draw() {
+  background("lightgreen")
+  
+  if(keyDown("space")&&monkey.y >= 350){
+    monkey.velocityY=-10
+  }
+  monkey.velocityY = monkey.velocityY + 0.3
+  monkey.collide(ground)
+  
+  
+  ground.velocityX = -9
+ ground.x = ground.width/2;
+    
+ if(World.frameCount%200===0){
+    fruits()
+ }
+  
+  if(World.frameCount%300===0){
+    stones()
+ }
+  
+  if(monkey.isTouching(FoodGroup)){
+     FoodGroup.destroyEach()
+    score=score+1
+      }
+  
+ 
+ drawSprites()
+  fill("white") 
+  text("Score: "+ score, 500,50);
+  
+  fill("black")
+  var survivalTime=Math.round(getFrameRate()/1);
+  text("Survival Time: "+ survivalTime,350,50)
+  
+}
+
+function fruits(){
+  banana=createSprite(670,Math.round(random(170,230)),10,10)
+  banana.addImage(bananaImage)
+  banana.scale=0.1
+  banana.velocityX=-9
+  FoodGroup.add(banana)
+}
+
+function stones(){
+  obstacle=createSprite(670,380,10,10)
+  obstacle.addImage(obstaceImage)
+  obstacle.velocityX=-14
+  obstacle.scale=0.2
+  obstacleGroup.add(obstacle)
+}
+
+
+
+
+
+
+
